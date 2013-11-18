41fa63370a9434ceef61f7f8850e4208fbf66c26
Daniel Carter <geddoe@gmail.com>
2013-11-18 11:59:42 -0500

    Made algorithm slightly more effecient.

--- montyhall.rb
+++ montyhall.rb
@@ -6,16 +6,18 @@ run = gets.chomp.to_i
 
 stay_correct = 0
 switch_correct = 0
+time = Time.now
 run.times do
-  doors = ["goat","goat","car"].shuffle
+  doors = ["goat","goat","goat"]
+  car_door = Random.rand(3)
+  doors[car_door] = 'car'
   choice = Random.rand(3)
-  show_door = 0
-  switch_door = 0
-  (0..2).each{|door| show_door = door if doors[door] == 'goat' && door != choice }
-  (0..2).each {|door| switch_door = door if door != choice && door != show_door }
+  show_door = ([0,1,2] - [car_door,choice]).first
+  switch_door = ([0,1,2] - [show_door,choice]).first
   stay_correct += 1 if doors[choice] == 'car'
   switch_correct += 1 if doors[switch_door] == 'car'
 end
+puts Time.now - time
 
 stay_percent = (stay_correct.to_f / run) * 100
 switch_percent = (switch_correct.to_f / run) * 100
